3d3ecbc77598663d0fc34f7feaacb55de155912a
masyatya <masyatya@gmail.com>
2020-10-27 19:04:51 +0200

    Updates

---
+++ precache-manifest.62082996116528080524e47159b53748.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "667122ef930c13c968aec40ea45c8425",
+    "url": "/react_millionaire/index.html"
+  },
+  {
+    "revision": "455fbae549d234dd546e",
+    "url": "/react_millionaire/static/css/main.c61e1338.chunk.css"
+  },
+  {
+    "revision": "277675ffc0c53a54d686",
+    "url": "/react_millionaire/static/js/2.109e61d9.chunk.js"
+  },
+  {
+    "revision": "3c73c585782ac05880c0f89bcfdbba5a",
+    "url": "/react_millionaire/static/js/2.109e61d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "455fbae549d234dd546e",
+    "url": "/react_millionaire/static/js/main.a9ea2921.chunk.js"
+  },
+  {
+    "revision": "74cb91bc47340ac8f9d4",
+    "url": "/react_millionaire/static/js/runtime-main.348c0047.js"
+  },
+  {
+    "revision": "7ecf4ccd20b74fe74ae042a097843221",
+    "url": "/react_millionaire/static/media/logo.7ecf4ccd.png"
+  }
+]);
